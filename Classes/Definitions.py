c861192e3e4136615505f29c45190b4ddca27529
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-10-26 19:19:16 -0300

    mudanças em definitions e em Menu

---
+++ Classes/Definitions.py
@@ -0,0 +1,9 @@
+import os 
+import json
+import nltk
+import string
+import networkx as nx
+import matplotlib.pyplot as plt
+from IPython.display import Image, display
+from Artigos import Artigos
+from Menu import Menu
